1f29d8fea22257d1e651bc6b69d72163f3e2b846
Trevor Sherrard <sherrardTr@gmail.com>
2016-11-15 17:26:39 -0500

    Update XBEE_Wireless_Motor_Control.ino
    changed control scheme to use "directional control" characters

--- ArduinoCode/XBEE_Wireless_Motor_Control/XBEE_Wireless_Motor_Control.ino
+++ ArduinoCode/XBEE_Wireless_Motor_Control/XBEE_Wireless_Motor_Control.ino
@@ -9,22 +9,16 @@
 #define XBEE Serial1
 
 //motor pin constants
-int MLPWMpin = 5;
-int MRPWMpin = 6;
+int MLPWMPin = 5;
+int MRPWMPin = 6;
 int MLDirPin = 4;
 int MRDirPin = 7;
 int D2pin = 3;
 int SFpin = 2;
 
-String strLDir = "";
-String strRDir = "";
-String strLVel = "";
-String strRVel = "";
+int MotorSpeed = 128;
 
-int LDir = 0;
-int RDir = 0;
-int LVel = 0;
-int RVel = 0;
+char SerChar = 0;
 
 void setup(){
   Serial.begin(9600);
@@ -33,88 +27,55 @@ void setup(){
   pinMode(SFpin, INPUT);
   pinMode(D2pin, OUTPUT);
   pinMode(MLDirPin, OUTPUT);
-  pinMode(MLPWMpin, OUTPUT);
+  pinMode(MLPWMPin, OUTPUT);
   pinMode(MRDirPin, OUTPUT);
-  pinMode(MRPWMpin, OUTPUT);
+  pinMode(MRPWMPin, OUTPUT);
 } 
 
 void loop(){
   //see if there is anything in serial buffer
   if(XBEE.available() > 0)
   {
-    /*commands should be sent over in the format:
-     * LDir,RDir,LVel,RVel
-     * No spaces between; and null terminated String format
-     */
-    strLDir = XBEE.readStringUntil(',');
-    XBEE.read();
-    strRDir = XBEE.readStringUntil(',');
-    XBEE.read();
-    strLVel = XBEE.readStringUntil(',');
-    XBEE.read();
-    strRVel = XBEE.readStringUntil('\0');
+    SerChar = XBEE.read();
+    XBEE.println("Got Full Command");
   }
   //Enable Motors
   digitalWrite(D2pin, HIGH);
-  //conver from string to int
-  LDir = strLDir.toInt();
-  RDir = strRDir.toInt();
-  LVel = strLVel.toInt();
-  RVel = strRVel.toInt();
-  //Left Vel bound checks
-  if(LVel < 0)
-  {
-    LVel = 0;
-  }
-  else if(LVel > 255)
-  {
-    LVel = 255;
-  }
 
-  //Right Vel bounds checks
-  if(RVel < 0)
+  // direction values for specific motors may need to be flipped. 
+  // The directions are assumed currently; they are not tested
+  if(SerChar == 'r')
   {
-    RVel = 0;
+    //go right
+    digitalWrite(MRDirPin, HIGH);
+    digitalWrite(MLDirPin, LOW);
+    analogWrite(MRPWMPin, MotorSpeed);
+    analogWrite(MLPWMPin, MotorSpeed);
   }
-  else if(RVel > 255)
+  else if(SerChar == 'l')
   {
-    RVel = 255;
+    //go left
+    digitalWrite(MRDirPin, LOW);
+    digitalWrite(MLDirPin, HIGH);
+    analogWrite(MRPWMPin, MotorSpeed);
+    analogWrite(MLPWMPin, MotorSpeed);
   }
-
-  //Left Motor Direction Control
-  if(LDir != 0)
+  else if(SerChar == 'f')
   {
-    if(LDir = -1)
-    {
-      digitalWrite(MLDirPin, HIGH);
-    }
-    else if(LDir = 1)
-    {
-      digitalWrite(MLDirPin, HIGH);
-    }
+    //go forward
+    digitalWrite(MRDirPin, HIGH);
+    digitalWrite(MLDirPin, HIGH);
+    analogWrite(MRPWMPin, MotorSpeed);
+    analogWrite(MLPWMPin, MotorSpeed);
   }
-  //Right Motor Direction Control
-  if(RDir != 0)
+  else if(SerChar == 'b')
   {
-    if(RDir = -1)
-    {
-      digitalWrite(MRDirPin, HIGH);
-    }
-    else if(RDir = 1)
-    {
-      digitalWrite(MRDirPin, HIGH);
-    }
+    //go back
+    digitalWrite(MRDirPin, LOW);
+    digitalWrite(MLDirPin, LOW);
+    analogWrite(MRPWMPin, MotorSpeed);
+    analogWrite(MLPWMPin, MotorSpeed);
   }
-  //Write Vel values to motors
-  analogWrite(MLPWMpin, LVel);
-  analogWrite(MRPWMpin, RVel);
-
   //refresh delay
-  delay(100);
+  delay(50);
 }
-
-
-
-
-
-
