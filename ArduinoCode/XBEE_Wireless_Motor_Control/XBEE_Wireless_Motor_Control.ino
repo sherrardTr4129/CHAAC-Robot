fa8c9da074ccc08552323d9912b350ca4e6eb70d
Trevor Sherrard <sherrardTr@gmail.com>
2016-11-17 16:46:59 -0500

    Update XBEE_Wireless_Motor_Control.ino

--- ArduinoCode/XBEE_Wireless_Motor_Control/XBEE_Wireless_Motor_Control.ino
+++ ArduinoCode/XBEE_Wireless_Motor_Control/XBEE_Wireless_Motor_Control.ino
@@ -1,7 +1,7 @@
 /************************************************
  ** Author: Trevor Sherrard
  ** Partner: Rodney Sanchez
- ** Class: Robotics Systems 
+ ** Class: Robotics Systems
  ** Project: CHAAC unit robot
  ** Date: 11/18/2016
  ** File: XBEE_Wireless_Motor_Control.ino
@@ -16,11 +16,13 @@ int MRDirPin = 7;
 int D2pin = 3;
 int SFpin = 2;
 
-int MotorSpeed = 128;
+int RMotorSpeed, LMotorSpeed = 0;
+char DirChar;
+float LSerFloat = 0;
+float RSerFloat = 0;
 
-char SerChar = 0;
 
-void setup(){
+void setup() {
   Serial.begin(9600);
   XBEE.begin(9600);
   //motor pin assignments
@@ -30,56 +32,68 @@ void setup(){
   pinMode(MLPWMPin, OUTPUT);
   pinMode(MRDirPin, OUTPUT);
   pinMode(MRPWMPin, OUTPUT);
-} 
+}
 
-void loop(){
+void loop() {
   //set motor speed state back to 0 for next update
   analogWrite(MRPWMPin, 0);
   analogWrite(MLPWMPin, 0);
-  SerChar = 0;
   //see if there is anything in serial buffer
-  if(XBEE.available() > 0)
+  if (XBEE.available() > 0)
   {
-    SerChar = XBEE.read();
-    XBEE.println("ACK");
+    DirChar = XBEE.read();
+    if (DirChar == 'l')
+    {
+      LSerFloat = XBEE.parseFloat();
+      if (LSerFloat < 120)
+      {
+        digitalWrite(MLDirPin, LOW);
+        LMotorSpeed = map(int(LSerFloat), 0, 400, 255, 0);
+
+      }
+      else if (LSerFloat > 210)
+      {
+        digitalWrite(MLDirPin, HIGH);
+        LMotorSpeed = map(int(LSerFloat), 0, 400, 0, 255);
+
+      }
+      else if (LSerFloat < 220 && LSerFloat > 130)
+      {
+        LMotorSpeed = 0;
+      }
+    }
+    else if (DirChar == 'r') {
+      RSerFloat = XBEE.parseFloat();
+      if (RSerFloat < 150)
+      {
+        //right forward
+        digitalWrite(MRDirPin, LOW);
+        RMotorSpeed = map(int(RSerFloat), 0, 400, 255, 0);
+      }
+      else if (RSerFloat > 225)
+      {
+        //right reverse
+        digitalWrite(MRDirPin, HIGH);
+        RMotorSpeed = map(int(RSerFloat), 0, 400, 0, 255);
+
+      }
+      else if (RSerFloat < 220 && RSerFloat > 140)
+      {
+        RMotorSpeed = 0;
+      }
+    }
   }
-  //Enable Motors
-  digitalWrite(D2pin, HIGH);
-  Serial.println(SerChar);
-  // direction values for specific motors may need to be flipped. 
-  // The directions are assumed currently; they are not tested
-  if(SerChar == 'r')
-  {
-    //go right
-    digitalWrite(MRDirPin, HIGH);
-    digitalWrite(MLDirPin, LOW);
-    analogWrite(MRPWMPin, MotorSpeed);
-    analogWrite(MLPWMPin, MotorSpeed);
-  }
-  else if(SerChar == 'l')
-  {
-    //go left
-    digitalWrite(MRDirPin, LOW);
-    digitalWrite(MLDirPin, HIGH);
-    analogWrite(MRPWMPin, MotorSpeed);
-    analogWrite(MLPWMPin, MotorSpeed);
-  }
-  else if(SerChar == 'f')
-  {
-    //go forward
-    digitalWrite(MRDirPin, HIGH);
-    digitalWrite(MLDirPin, HIGH);
-    analogWrite(MRPWMPin, MotorSpeed);
-    analogWrite(MLPWMPin, MotorSpeed);
-  }
-  else if(SerChar == 'b')
-  {
-    //go back
-    digitalWrite(MRDirPin, LOW);
-    digitalWrite(MLDirPin, LOW);
-    analogWrite(MRPWMPin, MotorSpeed);
-    analogWrite(MLPWMPin, MotorSpeed);
-  }
-  //refresh delay
-  delay(50);
+  analogWrite(MRPWMPin, RMotorSpeed);
+  analogWrite(MLPWMPin, LMotorSpeed);
+
+  Serial.print("Speed L, MotorDir: " );
+  Serial.print(LMotorSpeed);
+  Serial.print(",");
+  Serial.println(digitalRead(MLDirPin));
+  Serial.print("Speed R, MotorDir: " );
+  Serial.print(RMotorSpeed);
+  Serial.print(",");
+  Serial.println(digitalRead(MRDirPin));
+  Serial.println();
+  delay(5);
 }
