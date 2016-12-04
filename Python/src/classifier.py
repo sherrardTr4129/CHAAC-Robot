8b41ce6549061a20c317e4f15ad18172fe3b6014
Trevor Sherrard <sherrardtr@gmail.com>
2016-12-04 02:08:49 +0000

    Update classifier.py

--- Python/src/classifier.py
+++ Python/src/classifier.py
@@ -3,24 +3,29 @@
 Created on Wed Nov 16 18:32:26 2016
 
 @author: deepblue
+@author: sherrardtr4129
 """
 
 import pandas as pd
 import numpy as np
+import serial
 from sklearn import linear_model, datasets
 
-dataseta = pd.read_csv('~/CHAAC_Code/Python/data/weather_data.csv', header =-1)
-dataset=dataseta.as_matrix()
-data=np.zeros((365,8))
-data= dataset
-training_data=np.zeros((365,3))
-trainingdata = data[:,[3,4,5]]
-trainingdata=trainingdata.astype(float)
-labels=np.zeros((365,1))
-labels=data[:,[6]]
-labels=labels.astype(float)
-logreg = linear_model.LogisticRegression(C=1e5)
-logreg.fit(trainingdata, labels)
-testpoint=np.zeros((1,3))
-testpoint=[22,73,30]
-expected=logreg.predict(testpoint)
+
+def ClassifyPoint(SummedPoint):
+     dataseta = pd.read_csv("/home/sherrardtr/CHAAC_Code/Python/data/weather_data.csv", header =-1)
+     dataset=dataseta.as_matrix()
+     data=np.zeros((365,8))
+     data= dataset
+     training_data=np.zeros((365,3))
+     trainingdata = data[:,[3,4,5]]
+     trainingdata=trainingdata.astype(float)
+     labels=np.zeros((365,1))
+     labels=data[:,[6]]
+     labels=labels.astype(float)
+     logreg = linear_model.LogisticRegression(C=1e5)
+     logreg.fit(trainingdata, labels)
+     testpoint=np.zeros((1,3))
+     testpoint=SummedPoint
+     expected=logreg.predict(testpoint) 
+     return expected
