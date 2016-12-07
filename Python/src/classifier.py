3b7c6a2f823f68025a5e0365eada3b1478eed4d5
Trevor Sherrard <sherrardtr@gmail.com>
2016-12-07 18:32:49 -0500

    Update classifier.py

--- Python/src/classifier.py
+++ Python/src/classifier.py
@@ -9,17 +9,10 @@ Created on Wed Nov 16 18:32:26 2016
 import pandas as pd
 import numpy as np
 import serial
-import RPi.GPIO as GPIO
 from sklearn import linear_model, datasets
 
-#constants
-ExpectedValue = 0
-#need to change this pin number
-TeensyInterruptPin = 0
-
-#attach "Interrupts" to Pin for teensy to start 
-#weather data collection and classification
-GPIO.add_event_detect(TeensyInterruptPin, GPIO.RISING)
+port = 'dev/ttyACMO'
+Serial = serial.Serial(port, 9600, timeout=5)
 
 def ClassifyPoint():
      dataseta = pd.read_csv("/home/sherrardtr/CHAAC_Code/Python/data/weather_data.csv", header =-1)
@@ -36,8 +29,9 @@ def ClassifyPoint():
      logreg.fit(trainingdata, labels)
      testpoint=np.zeros((1,3))
      testpoint=[10,24,30]
-     expected=logreg.predict(testpoint) 
+     expected=logreg.predict(testpoint)
      ExpectedValue = expected
 
-GPIO.add_event_callback(TeensyInterruptPin, ClassifyPoint)
+while(True):
+     msg = Serial.readline()
 
