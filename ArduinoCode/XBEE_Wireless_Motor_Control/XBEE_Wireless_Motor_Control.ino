20d7ff99bb264b67788057412a65a75fd8dc79a5
Trevor Sherrard <sherrardTr@gmail.com>
2016-12-09 02:32:25 -0500

    Update XBEE_Wireless_Motor_Control.ino

--- ArduinoCode/XBEE_Wireless_Motor_Control/XBEE_Wireless_Motor_Control.ino
+++ ArduinoCode/XBEE_Wireless_Motor_Control/XBEE_Wireless_Motor_Control.ino
@@ -1,3 +1,4 @@
+
 /************************************************
  ** Author: Trevor Sherrard
  ** Partner: Rodney Sanchez
@@ -6,14 +7,20 @@
  ** Date: 11/18/2016
  ** File: XBEE_Wireless_Motor_Control.ino
  *************************************************/
+#include <stdint.h>
+#include "SparkFunBME280.h"
 #include "QuadDecoder.h"
-#include <ADC.h>
+#include "SPI.h"
+#include "Wire.h"
 #define mod 2249  // Number of ticks per wheel revolution
 #define XBEE Serial1
+
 //function Prototypes
 int readQD(int LineSensePin);
 long microsecondsToCentimeters(long microseconds);
 long GetUltrasonicTOF();
+void readandupdate(float T, float HU, float PR);
+
 void calcVelB(void);
 void calcVelA(void);
 void CloudyDance(void);
@@ -41,7 +48,7 @@ volatile int diffB = 0; // difference between newCntB and oldCntB
 //Conversion Constants
 const double rpmConvert =  2.667; // rev/min = cnt/us * us/sec * sec/min * rev/cnt
 const int period = 10000; // in microseconds
-int LineThreshold = 600;
+int LineThreshold = 350;
 
 //motor pin constants
 const int MLPWMPin = 5;
@@ -64,161 +71,24 @@ float LSerFloat = 0;
 float RSerFloat = 0;
 
 //Mode State Variable
-bool isWeatherMode = false;
+bool isWeatherMode = true;
 
 //Predicted Weather Character
 char WeatherDanceChar = 0;
 //Sparkfun BME280 Setup
+BME280 mySensor;
 
-	//***Driver settings********************************//
-	//commInterface can be I2C_MODE or SPI_MODE
-	//specify chipSelectPin using arduino pin names
-	//specify I2C address.  Can be 0x77(default) or 0x76
-	
-	//For I2C, enable the following and disable the SPI section
-	mySensor.settings.commInterface = I2C_MODE;
-	mySensor.settings.I2CAddress = 0x77;
-	
-	//For SPI enable the following and dissable the I2C section
-	//mySensor.settings.commInterface = SPI_MODE;
-	//mySensor.settings.chipSelectPin = 10;
+//averages
+float PRA = 0.0;
+float HUA = 0.0;
+float TA = 0.0;
 
+//current readings
+float T = 0.0;
+float HU = 0.0;
+float PR = 0.0;
 
-	//***Operation settings*****************************//
-	
-	//renMode can be:
-	//  0, Sleep mode
-	//  1 or 2, Forced mode
-	//  3, Normal mode
-	mySensor.settings.runMode = 3; //Normal mode
-	
-	//tStandby can be:
-	//  0, 0.5ms
-	//  1, 62.5ms
-	//  2, 125ms
-	//  3, 250ms
-	//  4, 500ms
-	//  5, 1000ms
-	//  6, 10ms
-	//  7, 20ms
-	mySensor.settings.tStandby = 0;
-	
-	//filter can be off or number of FIR coefficients to use:
-	//  0, filter off
-	//  1, coefficients = 2
-	//  2, coefficients = 4
-	//  3, coefficients = 8
-	//  4, coefficients = 16
-	mySensor.settings.filter = 0;
-	
-	//tempOverSample can be:
-	//  0, skipped
-	//  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
-	mySensor.settings.tempOverSample = 1;
-
-	//pressOverSample can be:
-	//  0, skipped
-	//  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
-    mySensor.settings.pressOverSample = 1;
-	
-	//humidOverSample can be:
-	//  0, skipped
-	//  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
-	mySensor.settings.humidOverSample = 1;
-	
-	Serial.print("Program Started\n");
-	Serial.print("Starting BME280... result of .begin(): 0x");
-	
-	//Calling .begin() causes the settings to be loaded
-	delay(10);  //Make sure sensor had enough time to turn on. BME280 requires 2ms to start up.
-	Serial.println(mySensor.begin(), HEX);
-
-	Serial.print("Displaying ID, reset and ctrl regs\n");
-	
-	Serial.print("ID(0xD0): 0x");
-	Serial.println(mySensor.readRegister(BME280_CHIP_ID_REG), HEX);
-	Serial.print("Reset register(0xE0): 0x");
-	Serial.println(mySensor.readRegister(BME280_RST_REG), HEX);
-	Serial.print("ctrl_meas(0xF4): 0x");
-	Serial.println(mySensor.readRegister(BME280_CTRL_MEAS_REG), HEX);
-	Serial.print("ctrl_hum(0xF2): 0x");
-	Serial.println(mySensor.readRegister(BME280_CTRL_HUMIDITY_REG), HEX);
-
-	Serial.print("\n\n");
-
-	Serial.print("Displaying all regs\n");
-	uint8_t memCounter = 0x80;
-	uint8_t tempReadData;
-	for(int rowi = 8; rowi < 16; rowi++ )
-	{
-		Serial.print("0x");
-		Serial.print(rowi, HEX);
-		Serial.print("0:");
-		for(int coli = 0; coli < 16; coli++ )
-		{
-			tempReadData = mySensor.readRegister(memCounter);
-			Serial.print((tempReadData >> 4) & 0x0F, HEX);//Print first hex nibble
-			Serial.print(tempReadData & 0x0F, HEX);//Print second hex nibble
-			Serial.print(" ");
-			memCounter++;
-		}
-		Serial.print("\n");
-	}
-	
-	
-	Serial.print("\n\n");
-	
-	Serial.print("Displaying concatenated calibration words\n");
-	Serial.print("dig_T1, uint16: ");
-	Serial.println(mySensor.calibration.dig_T1);
-	Serial.print("dig_T2, int16: ");
-	Serial.println(mySensor.calibration.dig_T2);
-	Serial.print("dig_T3, int16: ");
-	Serial.println(mySensor.calibration.dig_T3);
-	
-	Serial.print("dig_P1, uint16: ");
-	Serial.println(mySensor.calibration.dig_P1);
-	Serial.print("dig_P2, int16: ");
-	Serial.println(mySensor.calibration.dig_P2);
-	Serial.print("dig_P3, int16: ");
-	Serial.println(mySensor.calibration.dig_P3);
-	Serial.print("dig_P4, int16: ");
-	Serial.println(mySensor.calibration.dig_P4);
-	Serial.print("dig_P5, int16: ");
-	Serial.println(mySensor.calibration.dig_P5);
-	Serial.print("dig_P6, int16: ");
-	Serial.println(mySensor.calibration.dig_P6);
-	Serial.print("dig_P7, int16: ");
-	Serial.println(mySensor.calibration.dig_P7);
-	Serial.print("dig_P8, int16: ");
-	Serial.println(mySensor.calibration.dig_P8);
-	Serial.print("dig_P9, int16: ");
-	Serial.println(mySensor.calibration.dig_P9);
-	
-	Serial.print("dig_H1, uint8: ");
-	Serial.println(mySensor.calibration.dig_H1);
-	Serial.print("dig_H2, int16: ");
-	Serial.println(mySensor.calibration.dig_H2);
-	Serial.print("dig_H3, uint8: ");
-	Serial.println(mySensor.calibration.dig_H3);
-	Serial.print("dig_H4, int16: ");
-	Serial.println(mySensor.calibration.dig_H4);
-	Serial.print("dig_H5, int16: ");
-	Serial.println(mySensor.calibration.dig_H5);
-	Serial.print("dig_H6, uint8: ");
-	Serial.println(mySensor.calibration.dig_H6);
-		
-	Serial.println();
-}
-  //averages
-  float PRA=0.0;
-  float HUA=0.0;
-  float TA=0.0;
-  
-  //current readings
-  float T = 0.0;
-  float HU = 0.0;
-  float PR = 0.0;
+int count = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -248,6 +118,145 @@ void setup() {
   pinMode(EchoPin, INPUT);
   pinMode(TriggerPin, OUTPUT);
   //Setup Sparkfun BME280
+  //***Driver settings********************************//
+  //commInterface can be I2C_MODE or SPI_MODE
+  //specify chipSelectPin using arduino pin names
+  //specify I2C address.  Can be 0x77(default) or 0x76
+
+  //For I2C, enable the following and disable the SPI section
+  mySensor.settings.commInterface = I2C_MODE;
+  mySensor.settings.I2CAddress = 0x77;
+
+  //For SPI enable the following and dissable the I2C section
+  //mySensor.settings.commInterface = SPI_MODE;
+  //mySensor.settings.chipSelectPin = 10;
+
+
+  //***Operation settings*****************************//
+
+  //renMode can be:
+  //  0, Sleep mode
+  //  1 or 2, Forced mode
+  //  3, Normal mode
+  mySensor.settings.runMode = 3; //Normal mode
+
+  //tStandby can be:
+  //  0, 0.5ms
+  //  1, 62.5ms
+  //  2, 125ms
+  //  3, 250ms
+  //  4, 500ms
+  //  5, 1000ms
+  //  6, 10ms
+  //  7, 20ms
+  mySensor.settings.tStandby = 0;
+
+  //filter can be off or number of FIR coefficients to use:
+  //  0, filter off
+  //  1, coefficients = 2
+  //  2, coefficients = 4
+  //  3, coefficients = 8
+  //  4, coefficients = 16
+  mySensor.settings.filter = 0;
+
+  //tempOverSample can be:
+  //  0, skipped
+  //  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
+  mySensor.settings.tempOverSample = 1;
+
+  //pressOverSample can be:
+  //  0, skipped
+  //  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
+  mySensor.settings.pressOverSample = 1;
+
+  //humidOverSample can be:
+  //  0, skipped
+  //  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
+  mySensor.settings.humidOverSample = 1;
+
+  Serial.print("Program Started\n");
+  Serial.print("Starting BME280... result of .begin(): 0x");
+
+  //Calling .begin() causes the settings to be loaded
+  delay(10);  //Make sure sensor had enough time to turn on. BME280 requires 2ms to start up.
+  Serial.println(mySensor.begin(), HEX);
+
+  Serial.print("Displaying ID, reset and ctrl regs\n");
+
+  Serial.print("ID(0xD0): 0x");
+  Serial.println(mySensor.readRegister(BME280_CHIP_ID_REG), HEX);
+  Serial.print("Reset register(0xE0): 0x");
+  Serial.println(mySensor.readRegister(BME280_RST_REG), HEX);
+  Serial.print("ctrl_meas(0xF4): 0x");
+  Serial.println(mySensor.readRegister(BME280_CTRL_MEAS_REG), HEX);
+  Serial.print("ctrl_hum(0xF2): 0x");
+  Serial.println(mySensor.readRegister(BME280_CTRL_HUMIDITY_REG), HEX);
+
+  Serial.print("\n\n");
+
+  Serial.print("Displaying all regs\n");
+  uint8_t memCounter = 0x80;
+  uint8_t tempReadData;
+  for (int rowi = 8; rowi < 16; rowi++ )
+  {
+    Serial.print("0x");
+    Serial.print(rowi, HEX);
+    Serial.print("0:");
+    for (int coli = 0; coli < 16; coli++ )
+    {
+      tempReadData = mySensor.readRegister(memCounter);
+      Serial.print((tempReadData >> 4) & 0x0F, HEX);//Print first hex nibble
+      Serial.print(tempReadData & 0x0F, HEX);//Print second hex nibble
+      Serial.print(" ");
+      memCounter++;
+    }
+    Serial.print("\n");
+  }
+
+
+  Serial.print("\n\n");
+
+  Serial.print("Displaying concatenated calibration words\n");
+  Serial.print("dig_T1, uint16: ");
+  Serial.println(mySensor.calibration.dig_T1);
+  Serial.print("dig_T2, int16: ");
+  Serial.println(mySensor.calibration.dig_T2);
+  Serial.print("dig_T3, int16: ");
+  Serial.println(mySensor.calibration.dig_T3);
+
+  Serial.print("dig_P1, uint16: ");
+  Serial.println(mySensor.calibration.dig_P1);
+  Serial.print("dig_P2, int16: ");
+  Serial.println(mySensor.calibration.dig_P2);
+  Serial.print("dig_P3, int16: ");
+  Serial.println(mySensor.calibration.dig_P3);
+  Serial.print("dig_P4, int16: ");
+  Serial.println(mySensor.calibration.dig_P4);
+  Serial.print("dig_P5, int16: ");
+  Serial.println(mySensor.calibration.dig_P5);
+  Serial.print("dig_P6, int16: ");
+  Serial.println(mySensor.calibration.dig_P6);
+  Serial.print("dig_P7, int16: ");
+  Serial.println(mySensor.calibration.dig_P7);
+  Serial.print("dig_P8, int16: ");
+  Serial.println(mySensor.calibration.dig_P8);
+  Serial.print("dig_P9, int16: ");
+  Serial.println(mySensor.calibration.dig_P9);
+
+  Serial.print("dig_H1, uint8: ");
+  Serial.println(mySensor.calibration.dig_H1);
+  Serial.print("dig_H2, int16: ");
+  Serial.println(mySensor.calibration.dig_H2);
+  Serial.print("dig_H3, uint8: ");
+  Serial.println(mySensor.calibration.dig_H3);
+  Serial.print("dig_H4, int16: ");
+  Serial.println(mySensor.calibration.dig_H4);
+  Serial.print("dig_H5, int16: ");
+  Serial.println(mySensor.calibration.dig_H5);
+  Serial.print("dig_H6, uint8: ");
+  Serial.println(mySensor.calibration.dig_H6);
+
+  Serial.println();
 
   //Enable Motors
   digitalWrite(D2pin, HIGH);
@@ -255,11 +264,13 @@ void setup() {
 }
 
 void loop() {
+
   Serial.print("isWeatherMode ?: ");
   Serial.println(isWeatherMode);
 
   Serial.print("DirChar is:");
   Serial.println(DirChar);
+
   if (XBEE.available() > 0)
   {
     DirChar = XBEE.read();
@@ -269,55 +280,67 @@ void loop() {
     }
   }
 
-  if (isWeatherMode)
+
+  if (!isWeatherMode)
   {
     int LineSensorVal = readQD(LineSensorPin);
     long Distance = GetUltrasonicTOF();
+    Serial.println("Read Sensors");
+
     //On the Line, step off to the right
+    Serial.print("LineSensorValue: ");
+    Serial.println(LineSensorVal);
+    Serial.print("Distance: ");
+    Serial.println(Distance);
     if (LineSensorVal < LineThreshold)
     {
+      Serial.println("In Right");
       digitalWrite(MLDirPin, HIGH);
       digitalWrite(MRDirPin, HIGH);
-      analogWrite(MRPWMPin, 128);
-      analogWrite(MLPWMPin, 128);
+      analogWrite(MRPWMPin, 0);
+      analogWrite(MLPWMPin, 160);
     }
     //Off the line, step on from the left
-    else
+    else if (LineSensorVal > LineThreshold)
     {
+      Serial.println("In Left");
       digitalWrite(MLDirPin, LOW);
       digitalWrite(MRDirPin, LOW);
-      analogWrite(MRPWMPin, 128);
-      analogWrite(MLPWMPin, 128);
+      analogWrite(MRPWMPin, 160);
+      analogWrite(MLPWMPin, 0);
+
     }
-    if (Distance < 8)
+    if (Distance < 15)
     {
       //turn right
-      digitalWrite(MLDirPin, LOW);
+      Serial.println("Too close!");
+      digitalWrite(MLDirPin, HIGH);
       digitalWrite(MRDirPin, HIGH);
       analogWrite(MRPWMPin, 128);
       analogWrite(MLPWMPin, 128);
-      delay(800);
+      delay(500);
       //go forward
       digitalWrite(MLDirPin, HIGH);
-      digitalWrite(MRDirPin, HIGH);
+      digitalWrite(MRDirPin, LOW);
       analogWrite(MRPWMPin, 255);
       analogWrite(MLPWMPin, 255);
-      delay(800);
+      delay(1500);
       //Turn Left
-      digitalWrite(MLDirPin, HIGH);
+      digitalWrite(MLDirPin, LOW);
       digitalWrite(MRDirPin, LOW);
       analogWrite(MRPWMPin, 255);
       analogWrite(MLPWMPin, 255);
       delay(800);
       //go forward Until Line Detected
       digitalWrite(MLDirPin, HIGH);
-      digitalWrite(MRDirPin, HIGH);
+      digitalWrite(MRDirPin, LOW);
       analogWrite(MRPWMPin, 128);
       analogWrite(MLPWMPin, 128);
-      while (readQD(LineSensorPin) < LineThreshold);
+      while (readQD(LineSensorPin) > LineThreshold);
     }
   }
-  else if (!isWeatherMode)
+
+  else if (isWeatherMode)
   {
     //set motor speed state back to 0 for next update
     analogWrite(MRPWMPin, 0);
@@ -390,6 +413,7 @@ void loop() {
     analogWrite(MRPWMPin, RMotorSpeed);
     analogWrite(MLPWMPin, LMotorSpeed);
   }
+
   Serial.print("Speed L, MotorDir: " );
   Serial.print(LMotorSpeed);
   Serial.print(",");
@@ -398,11 +422,6 @@ void loop() {
   Serial.print(RMotorSpeed);
   Serial.print(",");
   Serial.println(digitalRead(MRDirPin));
-  Serial.print("LineSensor Read value: ");
-  Serial.println(readQD(LineSensorPin));
-  Serial.println();
-  Serial.print("Distance");
-  Serial.println(GetUltrasonicTOF());
   Serial.println();
 }
 
@@ -427,29 +446,26 @@ long GetUltrasonicTOF()
   return microsecondsToCentimeters(duration);
 }
 
-void readandupdate(float T,float HU,float PR)
+void readandupdate(float T, float HU, float PR)
 {
-  PRA=0.0;
-  HUA=0.0;
-  TA=0.0;
+  PRA = 0.0;
+  HUA = 0.0;
+  TA = 0.0;
   Serial.print("Temperature: ");
-  while(count < 101)
- {
-  T=mySensor.readTempF();
-  PR=(mySensor.readFloatPressure())/3386.39;
-  HU=(mySensor.readFloatHumidity());
-  PRA =PR+PRA; 
-  HUA =HU+HUA;
-  TA=T+TA;
-  count=count+1;
-  Serial.println(TA, 2);
-  Serial.println(PRA, 2);
-  Serial.println(HUA, 2);
+  while (count < 101)
+  {
+    T = mySensor.readTempF();
+    PR = (mySensor.readFloatPressure()) / 3386.39;
+    HU = (mySensor.readFloatHumidity());
+    PRA = PR + PRA;
+    HUA = HU + HUA;
+    TA = T + TA;
+    count = count + 1;
   }
   count = 0.0;
-  PRA=(PRA/100.0);
-  HUA=(HUA/100.0);
-  TA = (TA/100.0);
+  PRA = (PRA / 100.0);
+  HUA = (HUA / 100.0);
+  TA = (TA / 100.0);
   Serial.print(mySensor.readTempF(), 2);
   Serial.print(TA, 2);
   Serial.println(" degrees F");
@@ -462,10 +478,8 @@ void readandupdate(float T,float HU,float PR)
   Serial.print("%RH: ");
   Serial.print(HUA, 2);
   Serial.println(" %");
-  
+
   Serial.println();
-  
-  delay(1000);
 }
 
 void calcVelA(void)
